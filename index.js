5c57ebf1b7e15d99fe9853e5ad1aa8e28b3d626e
REHANA DEVANI <Rehana@REHANAs-MacBook-Pro.local>
2015-11-29 20:07:50 -0600

    push new

--- index.js
+++ index.js
@@ -13,6 +13,48 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/json/:item', function(req, res) {
+    var user_id = req.params.item;
+
+    var str;
+    var MongoClient = require('mongodb').MongoClient;
+    var assert = require('assert');
+
+    var url = "mongodb://heroku_93gddv1q:i90vj85qr27o8a88106ql2glnj@ds049104.mongolab.com:49104/heroku_93gddv1q";
+    MongoClient.connect(url,function(err,db){
+      console.log("connected correctly to server");
+
+
+      var getById = function(db, callback) {
+        var cursor =db.collection('employees').find({"_id":parseInt(user_id)});
+       // console.log(cursor);
+        cursor.each(function(err, doc) {
+          assert.equal(err, null);
+          if (doc != null) {
+          //  console.dir(doc);
+          //  console.log("doc "+doc);
+            str=doc;
+          } else {
+
+            callback();
+          }
+        });
+
+      }
+
+      MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        getById(db, function() {
+
+          db.close();
+          res.json(str);
+        });
+      });
+
+    });
+
+
+});
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
